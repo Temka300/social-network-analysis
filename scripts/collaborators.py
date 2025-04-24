cfb7f324273197061b55d03c373dd234226c1b10
rylex27 <121716981+rylex27@users.noreply.github.com>
2025-04-24 16:23:27 +0800

    rylex27

--- scripts/collaborators.py
+++ scripts/collaborators.py
@@ -3,3 +3,6 @@ print("Hi by khulann11")
 print("Hi gooners by Gan-od and kyomoto07")
 print("hi by Ariuka and Ariuntungalag") 
 print("hi i'm togsoo by tuvshintugs and  ZeTogsoo")
+
+# Hiimel Oyun 4
+print("hi im from ohio by Zoljargal and rylex27")
