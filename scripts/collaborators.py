e46c0f45552c11c5c38d3ffda4608a4c0a206ec5
stillmeta <baterdene86290507@gmail.com>
2025-04-28 12:21:57 +0800

    by stillmeta

--- scripts/collaborators.py
+++ scripts/collaborators.py
@@ -3,6 +3,7 @@ print("Hi by khulann11")
 print("Hi gooners by Gan-od and kyomoto07")
 print("hi by Ariuka and Ariuntungalag") 
 print("hi i'm togsoo by tuvshintugs and  ZeTogsoo")
+print("Hi I love cs2 by Bat-Erdene and stillmeta")
 
 # Hiimel Oyun 4
 print("hi im from ohio by Zoljargal and rylex27")
