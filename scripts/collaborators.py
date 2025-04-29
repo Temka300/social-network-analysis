05c47891bfa7cff08f8044c37a1cccce45104898
mjamsran <jamsranmynganbaatar@gmail.com>
2025-04-29 09:59:35 +0800

    jamsran

--- scripts/collaborators.py
+++ scripts/collaborators.py
@@ -4,6 +4,7 @@ print("Hi gooners by Gan-od and kyomoto07")
 print("hi by Ariuka and Ariuntungalag") 
 print("hi i'm togsoo by tuvshintugs and  ZeTogsoo")
 print("Hi I love cs2 by Bat-Erdene and stillmeta")
+print("hi I love dota by Jamsran and mjamsran")
 
 # Hiimel Oyun 4
 print("hi im from ohio by Zoljargal and rylex27")
